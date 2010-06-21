705d1503f3b756aec908b43a7ac296a57353e0be
John McFarlane <John McFarlane@03cb304c-929a-b8d2-5531-aad0e9782889>
2010-06-21 15:38:46 +0000

    copied old collision code into relevant file.
    git-svn-id: https://crag.googlecode.com/svn/trunk@48 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/crag/physics/FormationBody.cpp
+++ src/crag/physics/FormationBody.cpp
@@ -28,3 +28,62 @@ bool physics::FormationBody::OnCollisionWithSphericalBody(SphericalBody & that_s
 {
 	return SphericalBody::OnCollisionWithSphericalBody(that_sphere);
 }
+
+
+
+
+#if 0
+class CollideSphereFunctor
+{
+public:
+	CollideSphereFunctor(Mesh & init_mesh, Sphere3f const & init_sphere) 
+	: mesh(init_mesh) 
+	, sphere(init_sphere)
+	, min_poly_area(init_sphere.Area() * 0.01f)
+	{ 
+	}
+	
+	bool IsInsideSurface(Vector3f const & j, Vector3f const & k, Vector3f const & l) const
+	{
+		Vector3f normal = TriangleNormal(j, k, l);
+		Normalize(normal);
+		return DotProduct(normal, sphere.center - k) < sphere.radius;
+	}
+	
+	bool CanTraverse(FormationNode const & node) const
+	{ 
+		Vector3f center = Vector3f::Origin();
+		Vector3f const & a = node.GetCorner(0).pos;
+		Vector3f const & b = node.GetCorner(1).pos;
+		Vector3f const & c = node.GetCorner(2).pos;
+		
+		if ( ! (IsInsideSurface(center, a, b) 
+				&& IsInsideSurface(center, b, c) 
+				&& IsInsideSurface(center, c, a)))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	bool CanElaborate(FormationNode const & node) const
+	{
+		Vector3f const & a = node.GetCorner(0).pos;
+		Vector3f const & b = node.GetCorner(1).pos;
+		Vector3f const & c = node.GetCorner(2).pos;
+		
+		return TriangleArea(a, b, c) >= min_poly_area;
+	}
+	
+	void operator()(Vertex & a, Vertex & b, Vertex & c, Vector3f const & normal)
+	{
+		mesh.AddFace(a, b, c, normal);
+	}
+	
+private:
+	Mesh & mesh;
+	Sphere3f const & sphere;
+	float min_poly_area;
+};
+#endif
