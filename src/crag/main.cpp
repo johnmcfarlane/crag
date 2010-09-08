c2eade050a18357272c61087d92cdf68ea86ee52
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2010-09-08 06:50:02 +0000

    Did I check that in? Wrapping sandbox code.
    git-svn-id: https://crag.googlecode.com/svn/trunk@166 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/crag/main.cpp
+++ src/crag/main.cpp
@@ -29,6 +29,7 @@
 namespace 
 {
 	bool Crag();
+	void Sandpit();
 }
 
 
@@ -37,6 +38,10 @@ namespace
 
 int main(int /*argc*/, char * * /*argv*/)
 {
+#if ! defined(NDEBUG)
+	Sandpit();
+#endif
+	
 	std::cout << "Crag Demo\n";
 	std::cout << "Copyright 2010 John McFarlane\n";
 	
@@ -99,5 +104,57 @@ namespace
 		}
 
 		return true;
+	}
+	
+	void Sandpit()
+	{
+		int const max_depth = 4;
+		int const array_size = 1 << max_depth;
+		float array [array_size];
+		ZeroArray (array, array_size);
+		
+		array [(array_size >> 2) * 0] = 0.0;
+		array [(array_size >> 2) * 2] = 1.0;
+		
+		int phase_depth = max_depth - 2;
+		for (int depth = max_depth - 1; depth > 0; -- depth)
+		{
+			int step = 1 << depth;
+			int stagger = step >> 1;
+			
+			for (int i = stagger; i < array_size; i += step)
+			{
+				int sample_indices [4] = 
+				{
+					(array_size + i - stagger - step) % array_size,
+					(array_size + i - stagger) % array_size,
+					(array_size + i + stagger) % array_size,
+					(array_size + i + stagger + step) % array_size
+				};
+				
+				float samples[4] = 
+				{
+					array[sample_indices[0]], 
+					array[sample_indices[1]], 
+					array[sample_indices[2]], 
+					array[sample_indices[3]]
+				};
+				
+				Assert(array[i] == 0);
+				if (depth >= phase_depth)
+				{
+					array[i] = (samples[1] + samples[2]) * .5;
+				}
+				else
+				{
+					array[i] = (samples[0] + samples[3]) * .5;
+				}
+			}
+		}
+		
+		for (int i = 0; i < array_size; ++ i)
+		{
+			std::cout << array[i] << '\n';
+		}
 	}
 }
