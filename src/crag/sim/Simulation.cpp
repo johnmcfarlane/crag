ffd4d0781ae397a32f24e4db7533e6965a0959f9
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2010-09-14 15:55:41 +0000

    New start point.
    git-svn-id: https://crag.googlecode.com/svn/trunk@177 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/crag/sim/Simulation.cpp
+++ src/crag/sim/Simulation.cpp
@@ -45,7 +45,7 @@ namespace
 	//////////////////////////////////////////////////////////////////////
 	// functions
 
-	sim::Vector3 const default_camera_pos(0,10000450,0);
+	sim::Vector3 const default_camera_pos(0,9997750,0);
 	CONFIG_DEFINE (use_default_camera_pos, bool, true);
 	CONFIG_DEFINE (camera_pos, sim::Vector3, default_camera_pos);
 	CONFIG_DEFINE (camera_rot, sim::Matrix4, static_cast<sim::Matrix4>(sim::Matrix4::Identity()));
