92b31661ec0db6b84bfb9b2f4d312444e918402f
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2010-09-18 01:12:12 +0000

    New template class, core::double_buffer, implements simple double buffering. Will be used first on the formation mesh and later on the scene/scene thread.
    git-svn-id: https://crag.googlecode.com/svn/trunk@183 03cb304c-929a-b8d2-5531-aad0e9782889

---
+++ src/crag/core/double_buffer.h
@@ -0,0 +1,101 @@
+/*
+ *  DoubleBuffer.h
+ *  crag
+ *
+ *  Created by John on 9/17/10.
+ *  Copyright 2009, 2010 John McFarlane. All rights reserved.
+ *  This program is distributed under the terms of the GNU General Public License.
+ *
+ */
+
+#pragma once
+
+
+namespace core
+{
+	
+	// Super-simple stl-style implementation of double-buffering.
+	// Really double-buffering is a subset of what this class can do.
+	// But the technique is best known for its application to buffers.
+	
+	template <typename T> class double_buffer
+	{
+	public:
+		// c'tor
+		double_buffer()
+		: front_buffer(buffers + 0)
+		, back_buffer(buffers + 1)
+		{
+		}
+		
+		double_buffer(double_buffer const & rhs)
+		{
+			set(rhs);
+		}
+		
+		// copy
+		double_buffer & operator = (double_buffer const & rhs)
+		{
+			set(rhs);
+			return * this;
+		}
+		
+		// front buffer accessors
+		T & front()
+		{
+			return * front_buffer;
+		}
+		
+		T const & front() const
+		{
+			return * front_buffer;
+		}
+		
+		// back buffer accessors
+		T & back()
+		{
+			return * back_buffer;
+		}
+		
+		T const & back() const
+		{
+			return * back_buffer;
+		}
+		
+		// index accessors
+		T & operator [] (int n)
+		{
+			Assert(n == 0 || n == 1);
+			return buffers [n];
+		}
+		
+		T const & operator [] (int n) const
+		{
+			Assert(n == 0 || n == 1);
+			return buffers [n];
+		}
+		
+		// flip the front and back buffers
+		void flip()
+		{
+			std::swap(front_buffer, back_buffer);
+		}
+		
+	private:
+		
+		void set(double_buffer const & that)
+		{
+			buffers[0] = that.buffers[0];
+			buffers[1] = that.buffers[1];
+			
+			int front_index = that.buffers - that.front_buffer;			
+			front_buffer = buffers + front_index;
+			back_buffer = buffers + (front_index ^ 1);
+		}
+		
+		T buffers[2];
+		T * front_buffer;
+		T * back_buffer;
+	};
+	
+}
