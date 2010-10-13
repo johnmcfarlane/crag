c98547279d7adaea7d1ea407df01c3bde3304327
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2010-10-13 01:14:40 +0000

    Adjusted core::for_each to be slightly more compliant. In case passing the first/last iterators as refs didn't already sound like a good idea, I highly recommend against it as first will come back the same as last.
    git-svn-id: https://crag.googlecode.com/svn/trunk@211 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/crag/core/for_each.h
+++ src/crag/core/for_each.h
@@ -18,15 +18,14 @@ namespace core
 	// 
 	// This is a reimplementation of std::for_each to compensate for the fact that 
 	// Visual C++'s implementation myseriously fails to not copy when passed functor refs.
-	// This causes a compiler error for chunk_functor_for_each which never did nothing to noone. 
-	// The irony is that MS's _For_each function writes over first, but it's f that causes the error.
+	// This causes a compiler error for chunk_functor_for_each which I don't have time to investigate. 
 	
 	template <typename ITERATOR, typename ITEM_FUNCTOR>
 	ITEM_FUNCTOR for_each (ITERATOR first, ITERATOR last, ITEM_FUNCTOR f)
 	{
-		for (ITERATOR i = first; i != last; ++ i)
+		for (; first != last; ++ first)
 		{
-			f (* i);
+			f (* first);
 		}
 		
 		return f;
