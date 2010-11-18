81dc787a06e37e2c7df71ae496f30fee8f7ee38b
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2010-11-18 01:39:35 +0000

    Tidied up main.cpp: - removed sandpit - moved all singletons into Crag(); main now prints a hello and returns the result of Crag - stats counter now instanced
    git-svn-id: https://crag.googlecode.com/svn/trunk@267 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/crag/main.cpp
+++ src/crag/main.cpp
@@ -16,6 +16,7 @@
 
 #include "core/ConfigEntry.h"
 #include "core/ConfigManager.h"
+#include "core/Statistics.h"
 
 #include "smp/ForEach.h"
 
@@ -29,7 +30,6 @@
 namespace 
 {
 	bool Crag();
-	void Sandpit();
 }
 
 
@@ -38,14 +38,8 @@ namespace
 
 int main(int /*argc*/, char * * /*argv*/)
 {
-#if ! defined(NDEBUG)
-	Sandpit();
-#endif
-	
 	std::cout << "Crag Demo\n";
 	std::cout << "Copyright 2010 John McFarlane\n";
-	
-	core::ConfigManager config_manager;
 	
 	return Crag() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
@@ -77,10 +71,15 @@ namespace
 
 	bool Crag()
 	{
+		// Instance the config manager first of all so that all the config variables, such as video_full_screen are correct.
+		core::ConfigManager config_manager;
+		
 		if (! sys::Init(Vector2i(video_resolution_x, video_resolution_y), video_full_screen, video_vsync, "Crag"))
 		{
 			return false;
 		}
+		
+		core::Statistics stat_manager;
 
 		smp::Init(0);
 
@@ -99,57 +98,5 @@ namespace
 		smp::Deinit();
 
 		return true;
-	}
-	
-	void Sandpit()
-	{
-		int const max_depth = 4;
-		int const array_size = 1 << max_depth;
-		float array [array_size];
-		ZeroArray (array, array_size);
-		
-		array [(array_size >> 2) * 0] = 0.0;
-		array [(array_size >> 2) * 2] = 1.0;
-		
-		int phase_depth = max_depth - 2;
-		for (int depth = max_depth - 1; depth > 0; -- depth)
-		{
-			int step = 1 << depth;
-			int stagger = step >> 1;
-			
-			for (int i = stagger; i < array_size; i += step)
-			{
-				int sample_indices [4] = 
-				{
-					(array_size + i - stagger - step) % array_size,
-					(array_size + i - stagger) % array_size,
-					(array_size + i + stagger) % array_size,
-					(array_size + i + stagger + step) % array_size
-				};
-				
-				float samples[4] = 
-				{
-					array[sample_indices[0]], 
-					array[sample_indices[1]], 
-					array[sample_indices[2]], 
-					array[sample_indices[3]]
-				};
-				
-				Assert(array[i] == 0);
-				if (depth >= phase_depth)
-				{
-					array[i] = (samples[1] + samples[2]) * .5f;
-				}
-				else
-				{
-					array[i] = (samples[0] + samples[3]) * .5f;
-				}
-			}
-		}
-		
-		for (int i = 0; i < array_size; ++ i)
-		{
-			std::cout << array[i] << '\n';
-		}
 	}
 }
