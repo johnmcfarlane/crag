b00e2fa5b084eb4473c64fc04839efc96c15cf45
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2011-05-18 15:05:53 +0000

    changed comments
    git-svn-id: https://crag.googlecode.com/svn/trunk@333 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/crag/smp/atomic.h
+++ src/crag/smp/atomic.h
@@ -3,12 +3,11 @@
  *  crag
  *
  *  Created by John on 10/26/10.
- *  Copyright 2009, 2010 John McFarlane. All rights reserved.
+ *  Copyright 2009-2011 John McFarlane. All rights reserved.
  *  This program is distributed under the terms of the GNU General Public License.
  *
  */
 
-// TODO: Replace with SDL_Atomic*
 
 #pragma once
 
