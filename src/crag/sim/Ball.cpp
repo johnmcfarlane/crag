b5ea6772ad4d0284d6f28d833b331c7e20d9e292
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2011-06-25 03:02:55 +0000

    Accidentally checked in project file in last commit. Adding the missing (empty) source files that fix this.
    git-svn-id: https://crag.googlecode.com/svn/trunk@370 03cb304c-929a-b8d2-5531-aad0e9782889

---
+++ src/crag/sim/Ball.cpp
@@ -0,0 +1,12 @@
+//
+//  Ball.cpp
+//  crag
+//
+//  Created by John McFarlane on 6/23/11.
+//	Copyright 2009-2011 John McFarlane. All rights reserved.
+//	This program is distributed under the terms of the GNU General Public License.
+//
+
+#include "pch.h"
+
+#include "Ball.h"
---
+++ src/crag/sim/Ball.h
@@ -0,0 +1,18 @@
+//
+//  Ball.h
+//  crag
+//
+//  Created by John McFarlane on 6/23/11.
+//	Copyright 2009-2011 John McFarlane. All rights reserved.
+//	This program is distributed under the terms of the GNU General Public License.
+//
+
+#include "Entity.h"
+
+namespace sim
+{
+	class Ball : public Entity
+	{
+	public:
+	};
+}
