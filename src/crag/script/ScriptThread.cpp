d7bd9cfd050b268f327b2e6a9e23b8111d8b7ae0
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2011-10-05 04:31:09 +0000

    MINOR: Removing unused script invocation.
    git-svn-id: https://crag.googlecode.com/svn/trunk@649 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/crag/script/ScriptThread.cpp
+++ src/crag/script/ScriptThread.cpp
@@ -211,14 +211,6 @@ void script::ScriptThread::Run(Daemon::MessageQueue & message_queue)
 	}
 #endif
 
-#if ! defined(NDEBUG)
-	{
-		char const * fn = "./script/build_py_deps.py";
-		FILE * f = fopen(fn, "r");
-		PyRun_SimpleFileEx(f, fn, true);
-	}
-#endif
-	
 	PyRun_SimpleFileEx(_source_file, _source_filename, true);
 	
 	_message_queue->DispatchMessages(* this);
