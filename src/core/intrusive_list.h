6904eb98390b72251df03a81dcfa272b57eff462
mcfarlane.john@gmail.com <mcfarlane.john@gmail.com@03cb304c-929a-b8d2-5531-aad0e9782889>
2011-11-13 03:16:32 +0000

    WIN32: Fix to macro that defines intrusive_list types.
    git-svn-id: https://crag.googlecode.com/svn/trunk@736 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/core/intrusive_list.h
+++ src/core/intrusive_list.h
@@ -15,12 +15,12 @@
 #if defined(WIN32)
 	#define DEFINE_INTRUSIVE_LIST_TYPE(LISTED_CLASS, HOOK_MEMBER, LIST_TYPE) \
 		template <typename CLASS> struct _ListTypeDefinitionHelper { \
-		typedef core::intrusive::list<CLASS, & CLASS::_hook> list_type; }; \
-		typedef _ListTypeDefinitionHelper<LISTED_CLASS>::list_type list_type;
+		typedef core::intrusive::list<CLASS, & CLASS::_hook> LIST_TYPE; }; \
+		typedef _ListTypeDefinitionHelper<LISTED_CLASS>::LIST_TYPE LIST_TYPE;
 	#define DEFINE_TEMPLATED_INTRUSIVE_LIST_TYPE(LISTED_CLASS, HOOK_MEMBER, LIST_TYPE) \
 		template <typename CLASS> struct _ListTypeDefinitionHelper { \
-		typedef core::intrusive::list<CLASS, & CLASS::_hook> list_type; }; \
-		typedef typename _ListTypeDefinitionHelper<LISTED_CLASS>::list_type list_type;
+		typedef core::intrusive::list<CLASS, & CLASS::_hook> LIST_TYPE; }; \
+		typedef typename _ListTypeDefinitionHelper<LISTED_CLASS>::LIST_TYPE LIST_TYPE;
 #else
 	#define DEFINE_INTRUSIVE_LIST_TYPE(LISTED_CLASS, HOOK_MEMBER, LIST_TYPE) \
 		typedef core::intrusive::list<LISTED_CLASS, & LISTED_CLASS::HOOK_MEMBER> LIST_TYPE;
