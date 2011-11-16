dbccc9b364513765fd19bd3cfc4a4460907258ad
mcfarlane.john <mcfarlane.john@03cb304c-929a-b8d2-5531-aad0e9782889>
2011-11-16 07:09:21 +0000

    Replacing platform-specific versions of GetNumCpus() function with the nice tidy SDL version.
    git-svn-id: https://crag.googlecode.com/svn/trunk@753 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/smp/smp.cpp
+++ src/smp/smp.cpp
@@ -97,31 +97,5 @@ void smp::SetThreadName(char const * thread_name)
 
 int smp::GetNumCpus()
 {
-#if defined(WIN32)
-	SYSTEM_INFO sysinfo;
-	GetSystemInfo (& sysinfo);
-	return sysinfo.dwNumberOfProcessors;
-#elif defined(__APPLE__)
-	int num_cpus;
-    size_t len = sizeof(num_cpus);
-    int mib[2] = { CTL_HW, HW_NCPU };
-    if (sysctl(mib, 2, & num_cpus, &len, NULL, 0) == 0)
-	{
-		return num_cpus;
-	}
-	
-	Assert(false);
-	return 1;
-#else 
-    // Linux, Solaris, Tru64, UnixWare 7, and Open UNIX 8
-	// Assumes defined(_SC_NPROCESSORS_ONLN).
-    int num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-	if (num_cpus != -1)
-	{
-		return num_cpus;
-	}
-	
-	Assert(false);
-	return 1;
-#endif
+	return SDL_GetCPUCount();
 }
