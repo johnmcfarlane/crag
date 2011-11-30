601a07271642bee9d4af006ece90433fa3791d01
mcfarlane.john@gmail.com <mcfarlane.john@gmail.com@03cb304c-929a-b8d2-5531-aad0e9782889>
2011-11-30 06:32:38 +0000

    Always compile before committing.
    git-svn-id: https://crag.googlecode.com/svn/trunk@766 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/core/intrusive_list.h
+++ src/core/intrusive_list.h
@@ -307,9 +307,9 @@ namespace core
 			static void verify(value_type const & element)
 			{
 #if defined(VERIFY)
+				hook_type const & h = element.*Member;
 				VerifyPtr(h._next);
 				VerifyPtr(h._previous);
-				hook_type const & h = element.*Member;
 				VerifyTrue((h._next == nullptr) == (h._previous == nullptr));
 #endif
 			}
