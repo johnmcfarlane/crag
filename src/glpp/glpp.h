94a62fc79ef4c433d3f80f219bf074fa7bd4b7df
mcfarlane.john@gmail.com <mcfarlane.john@gmail.com@03cb304c-929a-b8d2-5531-aad0e9782889>
2011-12-19 05:34:46 +0000

    Added glpp support for glBlendFunc.
    git-svn-id: https://crag.googlecode.com/svn/trunk@804 03cb304c-929a-b8d2-5531-aad0e9782889

--- src/glpp/glpp.h
+++ src/glpp/glpp.h
@@ -118,6 +118,37 @@ namespace gl
 		return GetInt<GL_SHADE_MODEL>();
 	}
 	
+	// set/get blend function
+	enum BlendFactor
+	{
+		ZERO						= GL_ZERO, 
+		ONE							= GL_ONE, 
+		SRC_COLOR					= GL_SRC_COLOR, 
+		ONE_MINUS_SRC_COLOR			= GL_ONE_MINUS_SRC_COLOR, 
+		DST_COLOR					= GL_DST_COLOR, 
+		ONE_MINUS_DST_COLOR			= GL_ONE_MINUS_DST_COLOR, 
+		SRC_ALPHA					= GL_SRC_ALPHA, 
+		ONE_MINUS_SRC_ALPHA			= GL_ONE_MINUS_SRC_ALPHA, 
+		DST_ALPHA					= GL_DST_ALPHA, 
+		ONE_MINUS_DST_ALPHA			= GL_ONE_MINUS_DST_ALPHA, 
+		CONSTANT_COLOR				= GL_CONSTANT_COLOR, 
+		ONE_MINUS_CONSTANT_COLOR	= GL_ONE_MINUS_CONSTANT_COLOR, 
+		CONSTANT_ALPHA				= GL_CONSTANT_ALPHA, 
+		ONE_MINUS_CONSTANT_ALPHA	= GL_ONE_MINUS_CONSTANT_ALPHA,
+		SRC_ALPHA_SATURATE			= GL_SRC_ALPHA_SATURATE
+	};
+	
+	inline void BlendFunc(BlendFactor source, BlendFactor destination)
+	{
+		assert(destination != GL_SRC_ALPHA_SATURATE);
+		GLPP_CALL(glBlendFunc(source, destination));
+	}
+	inline void GetBlendFunc(BlendFactor & source, BlendFactor & destination)
+	{
+		source = static_cast<BlendFactor>(GetInt<GL_BLEND_SRC>());
+		destination = static_cast<BlendFactor>(GetInt<GL_BLEND_DST>());
+	}
+	
 	
 	////////////////////////////////////////////////////////////////////////////////
 	// Non-buffer poly rendering
