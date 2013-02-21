841157ccd9152552051fc9a773b02f5c39e1cf16
John McFarlane <mcfarlane.john@gmail.com>
2013-02-20 16:51:40 -0800

    adds missing file from previous RegulatorScript-related commit
    Former-commit-id: 79ff0da9e0f90f5b8c80ff296996c58ff267c376

---
+++ src/gfx/Messages.h
@@ -0,0 +1,28 @@
+//
+//  Messages.h
+//  crag
+//
+//  Created by John on 2013-02-19.
+//  Copyright 2013 John McFarlane. All rights reserved.
+//  This program is distributed under the terms of the GNU General Public License.
+//
+
+#pragma once
+
+namespace gfx
+{
+	struct NumQuaterneSetMessage
+	{
+		int num_quaterne;
+	};
+
+	struct FrameDurationSampledMessage
+	{
+		float frame_duration_ratio;
+	};
+
+	struct MeshGenerationPeriodSampledMessage
+	{
+		core::Time mesh_generation_period;
+	};
+}
