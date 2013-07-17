9b335954a66e1525c5f51950954ffada9b96281b
John McFarlane <mcfarlane.john@gmail.com>
2013-07-17 09:49:32 -0700

    fixes VC++ compiler warnings
    Former-commit-id: 865afc08b78d0dda746dfe5b96d904be741c16f4

--- src/gfx/object/Object.cpp
+++ src/gfx/object/Object.cpp
@@ -59,9 +59,9 @@ void Object::Verify() const
 	VerifyObject(_local_transformation);
 
 	auto scale = _local_transformation.GetScale();
-	VerifyTrue(NearEqual<Scalar>(scale.x, 1, 0.001));
-	VerifyTrue(NearEqual<Scalar>(scale.y, 1, 0.001));
-	VerifyTrue(NearEqual<Scalar>(scale.z, 1, 0.001));
+	VerifyTrue(NearEqual<Scalar>(scale.x, 1, 0.001f));
+	VerifyTrue(NearEqual<Scalar>(scale.y, 1, 0.001f));
+	VerifyTrue(NearEqual<Scalar>(scale.z, 1, 0.001f));
 }
 #endif
 
