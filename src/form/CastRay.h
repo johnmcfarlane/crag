51684339e062ab5586d45d742662441c4bdd7253
John McFarlane <john@mcfarlane.name>
2013-09-19 23:56:47 -0700

    clarifies interface to form::CastRay after realizing that positive results greater than length are possible
    Former-commit-id: c3229c96fdd7558476130b344c3b2686438bc5dc

--- src/form/CastRay.h
+++ src/form/CastRay.h
@@ -13,32 +13,33 @@ namespace form
 {
 	////////////////////////////////////////////////////////////////////////////////
 	// forward-declaration
-	
+
 	class Polyhedron;
 
 	////////////////////////////////////////////////////////////////////////////////
 	// RayCastResult class definition - represents contact between ray and polyhedron
-	
+
 	struct RayCastResult
 	{
 		// this type dictates the precision used in CastRay
 		typedef ::form::Scalar Scalar;
-		
+
 		// the normal of the surface with which the ray contacted
 		Vector3 normal;
-		
+
 		// the distance along the ray (as proportion of ray direction)
 		// at which contact occurred
 		Scalar projection = std::numeric_limits<Scalar>::max();
-		
+
 		// the individual node with which contact occurred
 		Node const * node = nullptr;
-			
+
 #if defined(VERIFY)
 		void Verify() const;
 #endif
 	};
 
-	// performs a ray cast on a polyhedron
+	// performs a ray cast on a polyhedron; ray must have unit direction 
+	// and length is the minimum guaranteed distance along ray to scan
 	RayCastResult CastRay(Polyhedron const & polyhedron, Ray3 const & ray, Scalar length);
 }
