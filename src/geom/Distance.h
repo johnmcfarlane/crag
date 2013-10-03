9ce4f20a4cd4ee7976c0af67d10cb6301750a98a
John McFarlane <john@mcfarlane.name>
2013-10-02 22:26:54 -0700

    adds geom::Distance overloads for cases where geometric objects are passed the other way around
    Former-commit-id: d63837cec56fd77bb1bd9937a83cb2dd518cd598

--- src/geom/Distance.h
+++ src/geom/Distance.h
@@ -35,29 +35,47 @@ namespace geom
 
 	// Distance from given surface, to point, p.
 	// Result is signed. If surface.direction isn't unit, result will be proportional.
-	template<typename S, int N> S Distance(Plane<S, N> const & surface, Vector<S, N> const & p) 
+	template<typename S, int N>
+	S Distance(Plane<S, N> const & surface, Vector<S, N> const & point) 
 	{
-		return DotProduct(surface.normal, p - surface.position);
+		return DotProduct(surface.normal, point - surface.position);
+	}
+	template<typename S, int N>
+	S Distance(Vector<S, N> const & point, Plane<S, N> const & surface)
+	{
+		return Distance(surface, point);
 	}
 
 	// Distance from plane represented by triangle, t, to point, p.
 	// Result is signed. 
-	template<typename S, int N> S Distance(Triangle<S, N> const & triangle, Vector<S, N> const & point) 
+	template<typename S, int N>
+	S Distance(Triangle<S, N> const & triangle, Vector<S, N> const & point)
 	{
 		Plane<S, N> plane(triangle);
 		Normalize(plane.normal);
 		
 		return Distance(plane, point);
 	}
+	template<typename S, int N>
+	S Distance(Vector<S, N> const & point, Triangle<S, N> const & triangle)
+	{
+		return Distance(triangle, point);
+	}
 
 	// Distance from plane represented by triangle, t, to point, p.
 	// Result is signed. Uses faster, less precise operations.
-	template<typename S, int N> S FastDistance(Triangle<S, N> const & triangle, Vector<S, N> const & point) 
+	template<typename S, int N> S
+	FastDistance(Triangle<S, N> const & triangle, Vector<S, N> const & point) 
 	{
 		Plane<S, N> plane;
 		plane.position = triangle.points[1];
 		plane.normal = FastNormalized(Normal(triangle));
 		
 		return Distance(plane, point);
+	}
+	template<typename S, int N> S
+	FastDistance(Vector<S, N> const & point, Triangle<S, N> const & triangle)
+	{
+		return FastDistance(point, triangle);
 	}
 }
