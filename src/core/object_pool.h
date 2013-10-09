4c8d8ae8cf91a35dfb37172fd845f7e54d9d31bd
John McFarlane <john@mcfarlane.name>
2013-10-09 01:09:09 -0700

    downgrades annoying assert to a debug message
    Former-commit-id: b2ddcf3b907f003e76b543e7ee6ffd40616cded7

--- src/core/object_pool.h
+++ src/core/object_pool.h
@@ -284,7 +284,10 @@ namespace core
 	
 		void deinit()
 		{
-			ASSERT(_num_allocated == 0);
+			if (_num_allocated != 0)
+			{
+				DEBUG_MESSAGE("object pool destroyed with " SIZE_T_FORMAT_SPEC " object remaining", _num_allocated);
+			}
 
 			FreePage(_array, get_allocation_size(capacity()));
 		}
