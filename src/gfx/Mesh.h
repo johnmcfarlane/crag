8c86b44af9293cd696871cfb56bd8533eb388271
John McFarlane <john@mcfarlane.name>
2014-01-30 13:13:29 -0800

    adds gfx::Mesh-related utility function templates, gfx::ForEachFace, gfx::CalculateVolume and gfx::CalculateCentroidAndVolume
    Former-commit-id: 0fcacae6ea7d07100760de4fe0449e1469ede244

--- src/gfx/Mesh.h
+++ src/gfx/Mesh.h
@@ -166,4 +166,88 @@ namespace gfx
 		
 		return GetBoundingRadius(mesh, scaler);
 	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	// ForEachFace
+	
+	// for each face, calls function with parameter, std::array<Vertex *, 3>;
+	// if mesh is non-const, function can take non-const reference instead 
+	template <typename Mesh, typename Function>
+	void ForEachFace(Mesh const & mesh, Function function)
+	{
+		CRAG_VERIFY(mesh);
+		
+		auto & vertices = mesh.GetVertices();
+		auto & indices = mesh.GetIndices();
+		
+		for (auto indices_iterator = std::begin(indices), indices_end = std::end(indices); indices_iterator != indices_end; )
+		{
+			// Parameter is passed to function
+			using Element = typename std::remove_reference<decltype(vertices.front())>::type;
+			using Parameter = std::array<Element *, 3>;
+		
+			Parameter parameter;
+			for (auto corner_index = 0u; corner_index != 3u; ++ indices_iterator, ++ corner_index)
+			{
+				std::size_t vertex_index = * indices_iterator;
+				parameter[corner_index] = & vertices[vertex_index];
+			}
+			
+			function(parameter);
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	// CalculateVolume
+	// 
+	// assumes mesh is simple, has limited overlapping and contains origin
+	
+	template <typename Vertex, typename IndexType>
+	Scalar CalculateVolume(Mesh<Vertex, IndexType> const & mesh)
+	{
+		auto volume = 0.f;
+		
+		// treat mesh as a collection of tetrahedrons with a point at origin
+		ForEachFace(mesh, [&] (std::array<Vertex const *, 3> face)
+		{
+			auto tetrahedron_volume = geom::TetrahedronVolume(face[0]->pos, face[1]->pos, face[2]->pos, Vector3::Zero());
+			
+			volume += tetrahedron_volume;
+		});
+		
+		return volume;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	// CalculateCentroidAndVolume
+	//
+	// same as CalculateVolume but additionally reports centroid
+	
+	template <typename Vertex, typename IndexType>
+	std::pair<Vector3, Scalar> CalculateCentroidAndVolume(Mesh<Vertex, IndexType> const & mesh)
+	{
+		auto centroid_and_volume = std::make_pair(Vector3::Zero(), 0.f);
+		
+		// treat mesh as a collection of tetrahedrons with a point at origin
+		ForEachFace(mesh, [&] (std::array<Vertex const *, 3> face)
+		{
+			Triangle3 triangle(face[0]->pos, face[1]->pos, face[2]->pos);
+			
+			// CalculateCentroid must be called with simple shape that contains origin
+			ASSERT(geom::Distance(triangle, Vector3::Zero()) < 0);
+			
+			auto triangle_centroid = geom::Centroid(triangle);
+			
+			constexpr auto two_thirds = Scalar(3) / Scalar(4);
+			auto tetrahedron_centroid = triangle_centroid * two_thirds;
+			auto tetrahedron_volume = geom::TetrahedronVolume(triangle.points[0], triangle.points[1], triangle.points[2], Vector3::Zero());
+			
+			// assumes that volume works in a weighted average
+			centroid_and_volume.first += tetrahedron_centroid * tetrahedron_volume;
+			centroid_and_volume.second += tetrahedron_volume;
+		});
+		
+		centroid_and_volume.first /= centroid_and_volume.second;
+		return centroid_and_volume;
+	}
 }
