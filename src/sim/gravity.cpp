f02f40fb5b794da800a94b9d4908a6f141942b2f
john <john@tp-debian>
2014-03-18 23:16:01 -0700

    increases gravity
    Former-commit-id: e2ff32476bb7febc658f977189d2f638a72c7514

--- src/sim/gravity.cpp
+++ src/sim/gravity.cpp
@@ -23,7 +23,7 @@ using namespace sim;
 
 namespace
 {
-	CONFIG_DEFINE (gravitational_force, float, 0.000000001f);
+	CONFIG_DEFINE (gravitational_force, float, 0.0000000025f);
 
 	// Given a position and a mass at that position, returns a 
 	// reasonable approximation of the weight vector at that position.
