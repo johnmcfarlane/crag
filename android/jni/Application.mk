d11cae9b78cd266fd87095803e69d7de7a2018db
John McFarlane <mcfarlane.john@gmail.com>
2015-01-05 01:18:55 -0800

    minor reorg of Application.mk
    Former-commit-id: 3db19bd71f67ef96d4abb6fd6e410d0cd186dd81

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,21 +1,13 @@
-# system stlport_static stlport_shared gnustl_static gnustl_shared gabi++_static gabi++_shared none
-#APP_STL := system	# fails on <algorithm>
-#APP_STL := stlport_static	# fails on <array>
-#APP_STL := stlport_shared	# fails on <array>
-APP_STL := gnustl_static	# success?
-#APP_STL := gnustl_shared	# success?
-#APP_STL := gabi++_static	# fails on <algorithm>
-#APP_STL := gabi++_shared	# fails on <algorithm>
-#APP_STL := none	# fails on <cassert>
-
 NDK_TOOLCHAIN_VERSION := 4.8
 #NDK_TOOLCHAIN_VERSION := clang
 
-#APP_ABI := armeabi armeabi-v7a x86
+#APP_ABI := all
 APP_ABI := armeabi-v7a
 
 APP_PLATFORM := android-14
+
 #APP_OPTIM := debug
 APP_OPTIM := release
-APP_CPPFLAGS += -std=c++11
 
+APP_CPPFLAGS += -std=c++11
+APP_STL := gnustl_static
