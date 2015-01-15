917940d4636a657e7f45dcb3bc78299209572d35
John McFarlane <mcfarlane.john@gmail.com>
2015-01-14 19:31:58 -0800

    adds essential Ubuntu LTS14.04 apt-get command for NaCL
    Former-commit-id: 26a7eb81a0d75459899d3f94d58229ce698938c6

--- nacl/setup.sh
+++ nacl/setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# system packages
+sudo apt-get install libc6:i386 git mercurial automake libtool
+
 # init
 LOCAL_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 SCRIPT_DIR="$LOCAL_DIR"/script
