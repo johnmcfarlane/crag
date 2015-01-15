72fea1233c80a1f31da63a1a501f4239a88802f3
John McFarlane <mcfarlane.john@gmail.com>
2015-01-14 22:34:53 -0800

    minor clean-up of SDL_image setup script
    Former-commit-id: 4c5a0f76d9d01e35d788086ccc39cd81743343ac

--- nacl/script/setup_sdl_image.sh
+++ nacl/script/setup_sdl_image.sh
@@ -13,21 +13,19 @@ hg pull http://hg.libsdl.org/SDL_image
 hg update
 
 # configure SDL_image
-export CFLAGS="-I$NACL_DIR/include/SDL2 -I$NACL_SDK_ROOT/include -I$NACL_SDK_ROOT/include/pnacl -I$NACL_SDK_ROOT/ports/include"
 export CC="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-clang"
 export AR="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-ar" 
 export LD="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-ar"
-export LDFLAGS="-L$NACL_DIR/lib -L$NACL_SDK_ROOT/ports/lib/newlib_pnacl/Release -L$NACL_SDK_ROOT/lib -L$NACL_SDK_ROOT/lib/pnacl -L$NACL_SDK_ROOT/lib/pnacl/Release"
-export LIBS="-lm -lppapi_gles2 -lppapi_simple -lSDL2 -lppapi_gles2"
+export LDFLAGS="-L$NACL_SDK_ROOT/lib/pnacl/Release"
 export RANLIB="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-ranlib"
-export SDL2_CONFIG="$NACL_SDK_ROOT"/bin/sdl2-config
+export SDL2_CONFIG="$NACL_DIR"/bin/sdl2-config
 
-#./autogen.sh
+./autogen.sh
 ./configure --host=pnacl --prefix "$NACL_DIR"
 
-make CONFIG="Release" || true
+make CONFIG="Release" -j"$NCPUS" || true
 
-# TODO: fix SDL_image WRT PNaCL
+# TODO: fix so make doesn't fail
 #make install
 cp "$SDL_IMAGE_DIR"/.libs/libSDL2_image.a "$NACL_DIR"/lib
 cp "$SDL_IMAGE_DIR"/.libs/libSDL2_image.la "$NACL_DIR"/lib
