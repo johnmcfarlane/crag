cc8a364d27a247b686a97fa4ddf553be5dced83d
John McFarlane <mcfarlane.john@gmail.com>
2015-01-14 19:31:22 -0800

    clean-ups to NaCL setup SDL script
    - automatic detection of num CPUs
    
    
    Former-commit-id: ff608390aa73a4616518443bf3bbe4ab41b78cdc

--- nacl/script/init.sh
+++ nacl/script/init.sh
@@ -4,3 +4,9 @@ SDK_NAME=pepper_39
 LOCAL_DIR="$( dirname "$0" )"
 NACL_DIR="$( cd "$LOCAL_DIR"/..; pwd )"
 NACL_SDK_ROOT="$NACL_DIR"/nacl_sdk/"$SDK_NAME"
+
+NCPUS="1"
+if [ -n `which nproc` ]; then
+    NCPUS=`nproc`
+fi  
+
--- nacl/script/setup_sdl.sh
+++ nacl/script/setup_sdl.sh
@@ -17,18 +17,9 @@ export CFLAGS="$CFLAGS -I$NACL_SDK_ROOT/include -I$NACL_SDK_ROOT/include/pnacl"
 export CC="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-clang"
 export AR="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-ar"
 export LD="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-ar"
-export LDFLAGS="-L$NACL_SDK_ROOT/lib"
-export LIBS="-lm"
 export RANLIB="$NACL_SDK_ROOT/toolchain/linux_pnacl/bin/pnacl-ranlib"
-export SDL2_CONFIG="$NACL_SDK_ROOT"/bin/sdl2-config
 
-#./autogen.sh
-./configure --host=pnacl --prefix "$NACL_DIR"
-make -j4
+"$SDL_DIR"/configure --host=pnacl --prefix "$NACL_DIR"
+./build-scripts/naclbuild.sh "$NACL_SDK_ROOT"
+make -j$NCPUS CFLAGS="$CFLAGS -I./include"
 make install
-
-#export CFLAGS="$CFLAGS -I$NACL_SDK_ROOT/include -I$NACL_SDK_ROOT/include/pnacl -I$NACL_SDK_ROOT/ports/include"
-#export LDFLAGS="-L$NACL_SDK_ROOT/ports/lib/newlib_pnacl/Release -L$NACL_SDK_ROOT/lib -L$NACL_SDK_ROOT/lib/pnacl/Release"
-#
-#export SDL2_CONFIG="$NACL_SDK_ROOT"/bin/sdl2-config
-#make CONFIG="Release"
