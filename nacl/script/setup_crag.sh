58eb0d4dcef2bccd0d605cceb9b213f54c9533ab
John McFarlane <mcfarlane.john@gmail.com>
2015-01-17 10:51:44 -0800

    adds Release flag to Crag makefile
    Former-commit-id: 37a6b5a2bed20810e1892b32ac21e606838f69bd

--- nacl/script/setup_crag.sh
+++ nacl/script/setup_crag.sh
@@ -6,7 +6,7 @@ LOCAL_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 source "$LOCAL_DIR"/init.sh
 
 # build
-make -j"$NCPUS"
+make -j"$NCPUS" CONFIG=Release $@
 
 # help
 echo run '"python -m SimpleHTTPServer"'
