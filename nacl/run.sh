af6f61ef87e7f3c0b46be920078f286628ec0c65
John McFarlane <mcfarlane.john@gmail.com>
2015-01-19 23:20:44 -0800

    improved NaCL debugging
    - run script launches Crag in browser, produces logging information
    - web files replaced with getting started p#2 setnacl_stdout.log
    
    
    Former-commit-id: d61e19a9493d734da5ec674d738ecd79e8e540c7

--- nacl/run.sh
+++ nacl/run.sh
@@ -3,18 +3,25 @@ set -e
 
 # init
 LOCAL_DIR="$( dirname "${BASH_SOURCE[0]}" )"
-source "$LOCAL_DIR"/script/init.sh
 CHROME_DIR=/opt/google/chrome/
 
+# where to put all of Chrome's files
+CHROME_DATA_DIR=$LOCAL_DIR/tmp
+mkdir -p $CHROME_DATA_DIR
+echo Chrome user are in $CHROME_DATA_DIR
+
+# chrome environment variables
 export NACL_PLUGIN_DEBUG=1
 export NACL_SRPC_DEBUG=[255]
 export NACLVERBOSITY=[255]
 
-export NACL_EXE_STDERR=~/nacl_stderr.log
-export NACL_EXE_STDOUT=~/nacl_stdout.log
+export NACL_EXE_STDERR="$LOCAL_DIR"/nacl_stderr.log
+export NACL_EXE_STDOUT="$LOCAL_DIR"/nacl_stdout.log
 export NACLLOG=~/nacl.log
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"$CHROME_DIR"
 
-"$CHROME_DIR"/chrome index.html --args --enable-logging --no-sandbox --vmodule=ppb*=4
-#python -m SimpleHTTPServer
+# launch chrome directly
+"$CHROME_DIR"/chrome --user-data-dir="$CHROME_DATA_DIR" --test-type --no-sandbox --args --enable-logging --vmodule=ppb*=4 http://localhost:8000/ &
+
+python -m SimpleHTTPServer
