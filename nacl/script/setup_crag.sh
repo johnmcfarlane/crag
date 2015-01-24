5ada08b1652ee5bec7d6dd1695c28775a63bc448
John McFarlane <mcfarlane.john@gmail.com>
2015-01-24 00:21:23 -0800

    removes hard-coded reference to NaCL version in nacl/Makefile
    Former-commit-id: 1a9fcd8cc8e358783354952da36c82c08235dcde

--- nacl/script/setup_crag.sh
+++ nacl/script/setup_crag.sh
@@ -6,4 +6,5 @@ LOCAL_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 source "$LOCAL_DIR"/init.sh
 
 # build
-make -j"$NCPUS" CONFIG=Release $@
+echo make -j"$NCPUS" CONFIG=Release $@ SDK_NAME=$SDK_NAME
+make -j"$NCPUS" CONFIG=Release $@ SDK_NAME=$SDK_NAME
