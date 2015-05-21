bd4aac9d187afbd45e488a4b8a0afa9bf21736ec
John McFarlane <mcfarlane.john@gmail.com>
2015-05-21 11:42:14 -0700

    removes disabled debug code

--- src/physics/Engine.cpp
+++ src/physics/Engine.cpp
@@ -384,13 +384,11 @@ void Engine::AddContacts(ContactGeom const * begin, ContactGeom const * end)
 
 		_contact.geom = contact_geom;
 		_contacts.push_back(_contact);
-		//std::cout << contact.geom.depth << ' ' << contact.geom.normal[0] << ',' << contact.geom.normal[1] << ',' << contact.geom.normal[2] << '\n';
 
 #if defined(DEBUG_CONTACTS)
 		Vector3 pos(Convert(contact_geom.pos));
 		Vector3 normal(Convert(contact_geom.normal));
 		gfx::Debug::AddLine(pos, pos + normal * contact_geom.depth * 100.f);
-		//std::cout << pos << ' ' << normal << ' ' << contact_geom.depth << '\n';
 #endif
 	});
 }
