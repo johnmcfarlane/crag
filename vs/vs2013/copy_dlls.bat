641bb472d8a2cf2e2fd975a0d5d9769f8e17d746
John McFarlane <mcfarlane.john@gmail.com>
2015-07-19 23:48:53 +0200

    MSVC project rewrite
    - project/solution now live in crag/vs/crag
    - removes remaining backward-compatability with VS2013 and earlier
    - updates to crag/vs/README.md

--- vs/vs2013/copy_dlls.bat
+++
@@ -1,4 +0,0 @@
-copy %1\SDL2\lib\x86\SDL2.dll %1\..
-copy %1\SDL2_image\lib\x86\SDL2_image.dll %1\..
-copy %1\ode\lib\DebugSingleDLL\ode_singled.dll %1\..
-copy %1\ode\lib\ReleaseSingleDLL\ode_single.dll %1\..
